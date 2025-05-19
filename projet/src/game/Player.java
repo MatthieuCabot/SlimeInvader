3c077c06cac6b5997727dacf992134b0ee465e04
Matthieu Cabot <matthieu.cabot3@etu.univ-lorraine.fr>
2025-05-19 22:26:51 +0200

    Création classe Player

--- projet/src/game/Player.java
+++ projet/src/game/Player.java
@@ -1,4 +1,76 @@
+
 package game;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class Player {
+
+    /**
+     * Attribut de Player
+     */
+    private double posX;
+    private double posY;
+
+    private double vY;
+
+    private ImageView sprite;
+    private Image[] sprites;
+
+    private static final double playerTaille = 40;
+
+    /**
+     * Constructeur de la classe Player
+     * @param width largeur de l'écran
+     * @param height hauteur de l'écran
+     */
+    public Player(double width, double height) {
+        this.posX = width / 2;
+        this.posY = height / 2;
+        this.vY = height;
+    }
+
+    /**
+     * Méthode de déplacement vers le haut
+     * @param height hauteur de l'écran
+     * @param seconde temps écouler avec le der,ier déplacement
+     */
+    public void deplacementHaut(double height, double seconde) {
+        posY = posY + vY * seconde;
+
+        if (posY > height) {
+            posY = height;
+        }
+    }
+
+    /**
+     * Méthode de déplacement vers le bas
+     * @param height
+     * @param seconde
+     */
+    public void deplacementBas(double height, double seconde) {
+        posY = posY - vY * seconde;
+
+        if (posY < 0) {
+            posY = 0;
+        }
+    }
+    
+    /**
+     * Guetter de l'attribut posX
+     * @return
+     */
+    public double getPosX() {
+        return posX;
+    }
+
+    /**
+     * Guetter de l'attribut posY
+     * @return
+     */
+    public double getPosY() {
+        return posY;
+    }
+
+
 }
