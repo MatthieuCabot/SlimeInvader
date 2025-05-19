bc039b7c1943f22aa12c9be45a2124a0870953b0
Matthieu Cabot <matthieu.cabot3@etu.univ-lorraine.fr>
2025-05-19 15:06:52 +0200

    initialisation du projet + gitignore

---
+++ projet/src/game/Main.java
@@ -0,0 +1,15 @@
+package game;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
